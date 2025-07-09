e26c974152e9c9b1647fe43585dfd6a05aa64f87
aasthaparanth <aasthap@vt.edu>
2025-07-09 17:15:22 +0530

    Connected Kong Manager UI to Kong APi Gateway

--- kong-custom/kong-plugin-grpc-transcode/README.md
+++
@@ -1,5 +0,0 @@
-# Kong gRPC-gateway plugin
-
-## :warning: This plugin is now maintained as part of [Kong](https://github.com/Kong/kong). Please open Issues and PRs in that repository
-
-## :open_book: For documentation, please visit https://docs.konghq.com/hub/kong-inc/grpc-gateway
--- kong-custom/kong/plugins/pre-function/handler.lua
+++ kong-custom/kong/plugins/pre-function/handler.lua
@@ -1,11 +1,13 @@
+local pre_validation = require "kong.plugins.pre-function.pre_validation"
+
 local plugin = {
   PRIORITY = 1000,
   VERSION = "1.0",
+  NAME = "pre-function"
 }
 
-function plugin.access(conf)
-  local validator = require "kong.plugins.pre-function.pre_validation"
-  return validator.validate()
+function plugin:access(conf)
+  pre_validation.validate()
 end
 
 return plugin
